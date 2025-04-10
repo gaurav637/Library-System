36522a6d890f595747de01ef7d679488445ae2b6
GauravNegi <negigaurav637@gmail.com>
2025-04-10 23:17:41 +0530

    fix(test): fix import issues

--- __tests__/author.resolver.spec.ts
+++ __tests__/author.resolver.spec.ts
@@ -4,9 +4,9 @@ import AppError from '../src/utils/AppError';
 import logger from '../src/utils/logger';
 import * as paginationModule from '../src/utils/pagination';
 
-jest.mock('../models/author.model');
-jest.mock('../utils/pagination');
-jest.mock('../utils/logger', () => ({
+jest.mock('../src/models/author.model');
+jest.mock('../src/utils/pagination');
+jest.mock('../src/utils/logger', () => ({
   info: jest.fn(),
   error: jest.fn(),
   warn: jest.fn()
